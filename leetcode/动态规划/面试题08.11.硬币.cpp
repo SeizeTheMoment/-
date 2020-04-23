38a8db40b2005646acaa0e6d8d7c7922776f0d92
SeizeTheMoment <1072118803@qq.com>
2020-04-23 18:39:16 +0800

    leetcode-动态规划

---
+++ leetcode/动态规划/面试题08.11.硬币.cpp
@@ -0,0 +1,18 @@
+const int MOD = 1e9+7;
+class Solution {
+public:
+    int waysToChange(int n) {
+          vector<long long> dp;
+          int val[4] = {25, 10, 5, 1};
+          dp.resize(n+1);
+          dp[0] = 1;
+          for(int x:val)
+          {
+              for(int i=x;i<=n;i++)
+              {
+                  dp[i] = (dp[i]+dp[i-x])%MOD;
+              }
+          }
+          return dp[n];
+    }
+};
